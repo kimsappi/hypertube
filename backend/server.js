a4e70256019b7b6f6aad2a2ac1eaba55e26da397
taho <timohive@gmail.com>
2020-09-11 21:57:56 +0300

    end of day 1

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,12 @@ var torrentStream = require('torrent-stream');
 // // connect to database
 // const pool = require("./config/db");
 
+// handles cross domain requests
+app.use(cors());
+
 // enable access to files in public folder
 app.use(express.static('public'));
 
-// handles cross domain requests
-app.use(cors());
 
 // // fixes image uploading size limit error
 // app.use(express.json({ limit: '2mb' }));
@@ -44,22 +45,64 @@ app.get('/api/cinema/:magnet',
 			engine.on('ready', () =>
 			{
 				let allFileNames = [];
-				let allStreams = [];
 
-				engine.files.forEach(file =>
-					{
-					allFileNames.push(file.name);
+				const file = engine.files[0];
+
+				// engine.files.forEach(file =>
+				// 	{
+				// 	allFileNames.push(file.name);
 					console.log('filename:', file.name);
+					console.log('file.length:', file.length);
 
-					// Create a readable stream to the file. Pieces needed by the stream will be prioritized highly.
-					var stream = file.createReadStream();
+					// This just pipes the read stream to the response object (which goes to the client) 
+					const head = {
+						'Accept-Ranges': 'bytes',
+						'Cache-Control': 'no-cache, no-store',
+						'Content-Range': `bytes 0-${file.length}/${file.length}`,
+						'Content-Length': file.length,
+						'Content-Type': 'video/mp4'
+					};
+					if (file.name.includes('mp4') || file.name.includes('mkv')) {
+						// Create a readable stream to the file. Pieces needed by the stream will be prioritized highly.
+						var stream = file.createReadStream();
+						res.writeHead(206, head);
+						console.log('streaming')
+						stream.pipe(res)
+					}
 
-					// console.log('stream:', stream);
-				});
+					// const parts = (range) ? range.replace(/bytes=/, '').split('-') : null
+					// const start = (parts) ? parseInt(parts[0], 10) : 0
+					// const end = (parts && parts[1]) ? parseInt(parts[1], 10) : file.length - 1
+
+					// res.writeHead(206, {
+					// 	'Accept-Ranges': 'bytes',
+					// 	'Cache-Control': 'no-cache, no-store',
+					// 	'Content-Range': `bytes ${start}-${end}/${file.length}`,
+					// 	'Content-Length': parseInt(end - start) + 1,
+					// 	'Content-Type': 'video/mp4'
+					// })
+
+				// 	//data: "ftypisomisomavc1,�moovlmvhd�(��(�X4 �@iodsO��)�Frtrak\tkhd�(��(�...
+
+				// 	// console.log('stream:', stream);
+				// });
 
 				// res.json(allFileNames);
-				res.json(engine.files[0].name);
+				// res.status(206);
+				//json(engine.files);
 			});
+
+			// Emitted when the metadata has been fetched.
+			engine.on('torrent', () => console.log("metadata has been fetched"));
+
+			// Emitted everytime a piece has been downloaded and verified.
+			engine.on('download', index =>
+			{
+				console.log(`part ${index} downloaded and verified`);
+			});
+
+			// Emitted when all selected files have been completely downloaded.
+			engine.on('idle', () => console.log("all selected files have been completely downloaded"));
 		}
 		catch (err)
 		{
--- frontend/src/components/pages/Cinema.js
+++ frontend/src/components/pages/Cinema.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect, Fragment } from "react";
 import { useParams } from 'react-router-dom';
 import axios from "axios";
 import ReactPlayer from "react-player";
+import Hls from 'hls.js';
 
 const Cinema = () =>
 {
 	const { magnet } = useParams();
-	const [stream, setStream] = useState("");
+	const [video, setVideo] = useState(null);
 	const [loading, setLoading] = useState(true);
 
 	useEffect(() =>
@@ -20,31 +21,63 @@ const Cinema = () =>
 				// console.log(magnet);
 
 				// fetch stream
-				const response = await axios.get("http://localhost:5000/api/cinema/" + magnet);
+				// const response = await axios.get("http://localhost:5000/api/cinema/" + magnet, {
+				// 	headers: {Accept: 'video/mp4'}
+				// });
+				// console.log(response);
 
-				console.log("response", response);
+				// console.log("response", response);  
+
+				// var video = document.getElementById('ReactPlayer');
+				// var videoSrc = 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8';
+				// // var videoSrc = "http://localhost:5000/South.Park.S23E07.HDTV.x264-SVA[ettv]/South.Park.S23E07.HDTV.x264-SVA[ettv].mkv";
+
+				// if (Hls.isSupported()) {
+				//   var hls = new Hls();
+				//   hls.loadSource(videoSrc);
+				//   hls.attachMedia(video);
+				//   hls.on(Hls.Events.MANIFEST_PARSED, function() {
+				// 		console.log('playing' + video);
+				// 		video.play();
+				//   });
+				// }
+				// console.log(hls); 
+				// setVideo(response.data); 
+				// console.log(response)
 				
-				setStream(response.data);
 				setLoading(false);
 			}
 			catch (err)
 			{
-				console.error(err.message);
+				console.log('asd');
+				console.error(err);
 			}
 		})()
 	}, [magnet]);
 
+// 	<ReactPlayer
+//   playing
+//   url={[
+//     {src: 'foo.webm', type: 'video/paska'},
+//     {src: 'foo.ogg', type: 'video/ogg'}
+//   ]}
+// />
+
 	return (
 		<Fragment>
+			{<ReactPlayer id="ReactPlayer" width="100%" playing={true} controls={true}
+			url={'http://localhost:5000/api/cinema/' + magnet} />}
+			{/* <video id="video"></video> */}
 			{loading && <div className="loading"></div>}
 			{!loading && (
 				<Fragment>
 					<div className="center p-5">
-						<h1>Hello, this is Mr. Backend.</h1>
-						<h4>Here's a file for you:</h4>
-						<p>{stream}</p>
+						{/* <p>{stream}</p> */}
 					</div>
-					<ReactPlayer width="100%" url="http://localhost:5000/A Bug's Life (1998)/A.Bugs.Life.1998.720p.BluRay.x264.YIFY.mp4" />
+					
+					{/* <ReactPlayer width="100%" playing={true} controls={true} url="http://localhost:5000/A Bug's Life (1998)/A.Bugs.Life.1998.720p.BluRay.x264.YIFY.mp4" /> */}
+					{/* <ReactPlayer width="100%" playing={true} controls={true} url="http://localhost:5000/South.Park.S23E07.HDTV.x264-SVA[ettv]/South.Park.S23E07.HDTV.x264-SVA[ettv].mkv" />
+					{video && <ReactPlayer width="100%" playing={true} controls={true} url={video} />} */}
 				</Fragment>
 			)}
 		</Fragment>
