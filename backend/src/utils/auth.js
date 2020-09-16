93bd7e18afb1987eab53ca49c12872537bec4552
Kim Sappi <ksappi@student.hive.fi>
2020-09-16 09:25:10 +0300

    Token checker middleware

--- backend/src/utils/auth.js
+++ backend/src/utils/auth.js
@@ -16,6 +16,30 @@ const generateEmailVerification = data => {
   return hash.digest('hex');
 }
 
+// Use this middleware on a router to make sure only logged in users can access
+// the endpoint. Will decode the user's token as req.user or return 401 if token
+// is invalid or not found.
+const authenticationMiddleware = (req, res, next) => {
+  try {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token)
+      return res.status(401).json('auth error');
+    else {
+      jwt.verify(token, config.TOKEN_SECRET, (err, user) => {
+        if (err)
+          return res.status(401).json('auth error');
+        else {
+          req.user = user;
+          next();
+        }
+      });
+    }
+  } catch(err) {
+    return res.status(401).json('auth error');
+  }
+};
+
 module.exports = {
   hashPassword,
   generateJWT,
