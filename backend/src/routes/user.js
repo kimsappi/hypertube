f70be000f1732d80c00eb176ea134bd6b6e0604c
Kim Säppi <sappikim@gmail.com>
2020-09-17 16:47:06 +0300

    Refactored /user endpoint

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -2,30 +2,36 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { authenticationMiddleware } = require('../utils/auth');
+const Logger = require('../utils/logger');
 
 router.get('/me/', authenticationMiddleware, async (req, res) => {
 	console.log(req.user.id);
-	console.log("get user data");
+	console.log("get my user data");
 
-	const userData = await User.find({
-		_id: req.user.id
-	},
-	'username firstname lastname email password profilePicture'
-	)
-	res.json(userData);
+	try {
+		const userData = await User.findById(req.user.id,
+			'username firstname lastname email password profilePicture'
+		);
+		res.json(userData);
+	} catch(err) {
+		Logger.error(err);
+		res.json(null);
+	}
 })
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', authenticationMiddleware, async (req, res) => {
 	console.log(req.params.id);
-	console.log("get user data");
+	console.log("get other user data");
 
-	const userData = await User.find({
-		_id: req.params.id
-	},
-	'username firstName lastName profilePicture'
-	)
-
-	res.json(userData);
+	try {
+		const userData = await User.findById(req.params.id,
+		'username firstName lastName profilePicture'
+		);
+		res.json(userData);
+	} catch(err) {
+		Logger.error(err);
+		res.json(null);
+	}
 })
 
 module.exports = router;
