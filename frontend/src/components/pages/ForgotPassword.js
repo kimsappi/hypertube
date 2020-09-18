ff56333100786aae4b7d66708ffbf16afa41d6fd
Kim Sappi <ksappi@student.hive.fi>
2020-09-18 12:56:12 +0300

    Password reset frontend

--- frontend/src/components/pages/ForgotPassword.js
+++ frontend/src/components/pages/ForgotPassword.js
@@ -1,13 +1,52 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+
+import config from '../../config/config'
 
 const ForgotPassword = () =>
 {
+	const [email, setEmail] = useState("");
+	const [error, setError] = useState("");
+	const [success, setSuccess] = useState("");
+
+	const changeEmail = event => {
+		setEmail(event.target.value);
+	};
+
+	const submitResetPassword = async event => {
+		event.preventDefault();
+		if (!event.target.checkValidity())
+			return;
+		try {
+			const res = await axios.post(config.SERVER_URL + '/api/auth/forgotPassword', {email});
+			if (res) {
+				setSuccess("Password reset email sent.");
+				setError("");
+			}
+		} catch(err) {
+			setError("Couldn't reset password.");
+			setSuccess("");
+		}
+	};
+
 	return (
 		<div className="center">
 			<h1 className="my-4">Forgot Password</h1>
-			<p>
-				Forgot password.
-			</p>
+			{error && <div className="small alert alert-error">{error}</div>}
+			<form onSubmit={submitResetPassword}>
+				<label htmlFor="email">Enter your account's email</label>
+				<input
+					className="mb-1"
+					type="email"
+					name="email"
+					placeholder="Account email"
+					autoComplete="on"
+					value={email}
+					onChange={changeEmail}
+					required={true}
+				/>
+				<button type="submit"><i className="fas color-black100"></i>Reset Password</button>
+			</form>
 		</div>
 	)
 }
