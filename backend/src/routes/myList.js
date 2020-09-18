0ae04221631cd14544682894b41de059026037c6
Kim Sappi <ksappi@student.hive.fi>
2020-09-18 16:12:14 +0300

    Fetching MyList from backend for MyList view

--- backend/src/routes/myList.js
+++ backend/src/routes/myList.js
@@ -8,13 +8,25 @@ const router = express.Router();
 
 router.post('/', authenticationMiddleware, async (req, res, next) => {
   try {
-    await movieListService.addToList(
+    const res = await movieListService.addToList(
       req.body.id,
       req.user,
       movieListService.Lists.myList
     );
+    return res.status(200).json(true);
   } catch(err) {
     Logger.error(err);
+    return res.status(400).json(false);
+  }
+});
+
+router.get('/', authenticationMiddleware, async (req, res, next) => {
+  try {
+    const response = await movieListService.getMyList(req.user.id);
+    return res.status(200).json(response);
+  } catch(err) {
+    Logger.error(err);
+    return res.status(400).json([]);
   }
 });
 
