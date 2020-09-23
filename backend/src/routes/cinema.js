78719fe872d6ccebaad027466bd8dfc6958295c7
josiz <jzaiedma@student.hive.fi>
2020-09-23 13:05:42 +0300

    added subtitles

--- backend/src/routes/cinema.js
+++ backend/src/routes/cinema.js
@@ -2,7 +2,8 @@ const express = require('express');
 const torrentStream = require('torrent-stream');
 
 const fs = require('fs');
-const srt2vtt = require('srt-to-vtt')
+const srt2vtt = require('srt2vtt');
+const glob = require('glob');
 
 const { verifyToken } = require('../utils/auth');
 const movieListService = require('../services/movieLists');
@@ -12,9 +13,89 @@ const Movie = require('../models/Movie');
 
 const router = express.Router();
 
-router.get('/:magnet/:token', async (req, res, next) => {
+router.get('/start/:magnet/:token/:imdb', async (req, res) => {
+  const { magnet, token, imdb } = req.params;
+  console.log(imdb);
+  console.log('start');
+  const engine = torrentStream("magnet:?" + magnet, {
+    tmp: "../public",
+    path: "../public/" + imdb,
+    trackers: [
+      "udp://glotorrents.pw:6969/announce",
+      "udp://tracker.opentrackr.org:1337/announce",
+      "udp://torrent.gresille.org:80/announce",
+      "udp://tracker.openbittorrent.com:80",
+      "udp://tracker.coppersurfer.tk:6969",
+      "udp://tracker.leechers-paradise.org:6969",
+      "udp://p4p.arenabg.ch:1337",
+      "udp://tracker.internetwarriors.net:1337"
+    ],
+  });
+let tries = 0;
+  engine.on('ready', () =>
+    {
+      engine.files.forEach(file =>
+        {
+          file.createReadStream();
+
+        })
+
+    })
+    engine.on('torrent', () => console.log("\033[35mmetadata has been fetched\033[0m"));
+    
+    engine.on('download', index => {
+      console.log("\033[36mpart " + index +  " downloaded and verified\033[0m"); 
+      // TAHAN VALIIN TEKSTITYSTEN TARKASTUS JA HAKU JA ALLA RES ANTAA URLIN TEKSTEIHIN.
+
+      //add one to tries on every download. If subs not found within 10 first downloads, response that no subs.
+      tries++;
+
+      //search for srt files. If found, fun cb.
+      glob('../public/' + imdb + '/*/*.srt', {}, (err, files) => {
+        console.log("LOYTYI: ");
+        console.log(files);
+        if (files.length)
+        {
+          //moving the subtitle file to imdb-folder.
+          fs.rename(files[0], '../public/' + imdb + '/sub.srt', (err) => { if(err) console.log(err); });
+
+          //convert srt2vtt. Timeout, that fs.rename has time to run..
+          setTimeout(() => {
+            var srtData = fs.readFileSync('../public/' + imdb + '/sub.srt');
+          
+            srt2vtt(srtData, (err, vttData) => {
+              if (err) console.log(err);
+              fs.writeFileSync('../public/' + imdb + '/sub.vtt', vttData);
+            })
+            console.log("LOYTYYYY");
+
+            
+          }, 1000);
+
+            engine.destroy();
+            console.log("jsut ennen sendia");
+            res.status(200).json({sub: imdb + '/sub.vtt', message: 'found'});
+            return;
+        }
+      })
+
+      if (tries === 20)
+      {
+        console.log("WTF");
+        engine.destroy();
+        return res.status(200).send("subtitles not fo");
+      }
+      console.log(tries);
+      //res.status(200).send("JEES");
+      
+  });
+    
+
+})
+
+router.get('/:magnet/:token/:imdb', async (req, res, next) => {
   try {
-    const { magnet, token } = req.params;
+    const { magnet, token, imdb } = req.params;
 	// const token = req.query.token;
 	
 	console.log("magnet", magnet);
@@ -53,7 +134,7 @@ router.get('/:magnet/:token', async (req, res, next) => {
     
       const engine = torrentStream("magnet:?" + magnet, {
         tmp: "../public",
-        path: "../public" ,
+        path: "../public/" + imdb,
         trackers: [
           "udp://glotorrents.pw:6969/announce",
           "udp://tracker.opentrackr.org:1337/announce",
@@ -109,11 +190,12 @@ router.get('/:magnet/:token', async (req, res, next) => {
           })
             .pipe(res);
           
+          
 		    }
         else
         {
-			file.createReadStream();
-		}
+          file.createReadStream();
+        }
 	  });
     });
 
@@ -121,7 +203,7 @@ router.get('/:magnet/:token', async (req, res, next) => {
     engine.on('torrent', () => console.log("\033[35mmetadata has been fetched\033[0m"));
 
     // // emitted everytime a piece has been downloaded and verified.
-	engine.on('download', index => console.log("\033[36mpart " + index +  " downloaded and verified\033[0m"));
+	engine.on('download', index => {console.log("\033[36mpart " + index +  " downloaded and verified\033[0m");});
 
     // emitted when all selected files have been completely downloaded.
     engine.on('idle', () => console.log("\033[0;32mall selected files have been completely downloaded\033[0m"));
--- frontend/src/components/pages/Cinema.js
+++ frontend/src/components/pages/Cinema.js
@@ -1,16 +1,19 @@
-import React, { useState, Fragment } from "react";
+import Axios from 'axios';
+import React, { useState, Fragment, useEffect } from "react";
 import { useParams } from 'react-router-dom';
 import ReactPlayer from "react-player";
 
 import config from '../../config/config';
 
-const Cinema = () =>
+const Cinema =  () =>
 {
-	const { magnet, imdb_id, title_long } = useParams();
+	const { magnet, title_long, imdb } = useParams();
 	const [status, setStatus] = useState("...");
 	const [secondsPlayed, setSecondsPlayed] = useState(0);
 	const [secondsLoaded, setSecondsLoaded] = useState(0);
 	const [totalSeconds, setTotalSeconds] = useState(0);
+	const [waitForIt, setWait] = useState(false);
+	const [subtitles, setSubtitles] = useState('test');
 	
 	const [movieReady, setMovieReady] = useState(false);
 	const [timeLeft, setTimeLeft] = useState(15);
@@ -31,10 +34,31 @@ const Cinema = () =>
 	const onBuffer = () => setStatus("BUFFERING");
 	const onEnded = () => setStatus("VIDEO END");
 	const onError = () => setStatus("ERROR");
-
+console.log("PARAM1:", magnet);
+console.log("IMDB:  ", imdb);
 	const language = "en";
 	const resolution = "720p";
-	const subtitleUrl = config.SERVER_URL + "/" + title_long + " [" + resolution + "] [WEBRip] [YTS.MX]/subs." + language + ".vtt";
+	let subtitleUrl = 'asdasd';
+
+	useEffect(async () => {const res = await Axios.get(config.SERVER_URL + "/api/cinema/start/" + magnet + "/" + token + "/" + imdb); 
+				console.log(res);
+					if (res.data == "subtitles not found" || res.data.message == "found")
+					{
+						if (res.data.message == 'found')
+						setTimeout(() => {
+							setSubtitles(config.SERVER_URL + "/" + res.data.sub);
+							setWait(true);
+						}, 1000);
+						
+						if (res.data == 'subtitles not found')
+						{
+							alert('no subtitles found. Playing anyways..');
+							setWait(true);
+						}
+					}
+					console.log(res);}, 
+					[])
+	
 
 	let count = setTimeout(() =>
 	{
@@ -52,9 +76,18 @@ const Cinema = () =>
 		}
 	}, 1000);
 
+	useEffect( () => {
+		
+		
+		
+	}, [waitForIt])
+
+console.log("WAITFOR: ", waitForIt);
 	return (
+		
 		<Fragment>
 			<div className="flex-center p-4 bg-black100">
+			{waitForIt === true ? 
 				<ReactPlayer
 					width="100%"
 					height="500px"
@@ -71,19 +104,20 @@ const Cinema = () =>
 					onBufferEnd={onPlay}
 					onEnded={onEnded}
 					onError={onError}
-					url={config.SERVER_URL + "/api/cinema/" + magnet + "/" + token}
+					url={config.SERVER_URL + "/api/cinema/" + magnet + "/" + token + "/" + imdb}
 					config={
 						{ file: {
 							attributes: {
 								crossOrigin: 'true'
 							},
 							tracks: [
-							{ kind: 'subtitles', src: subtitleUrl, srcLang: language, default: true } 
+							{ kind: 'subtitles', src: subtitles, srcLang: language, default: true } 
 							]
 							}
 						}
 					}
 				/>
+				: 'loading..'}
 			</div>
 			<div className="flex-column center">
 				<div>
@@ -95,6 +129,7 @@ const Cinema = () =>
 				<div>loaded: {Math.round(secondsLoaded)} / {Math.round(totalSeconds)} seconds</div>
 			</div>
 		</Fragment>
+		
 	)
 }
 
