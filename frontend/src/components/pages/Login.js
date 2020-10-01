92b23bce2408d2bdcd084299423be093d23dc54f
Kim Sappi <ksappi@student.hive.fi>
2020-10-01 11:09:32 +0300

    Removed logging, slightly nicer login page styling

--- frontend/src/components/pages/Login.js
+++ frontend/src/components/pages/Login.js
@@ -137,10 +137,9 @@ const Login = () =>
 						</div>
 						<div className="center mt-4">Don't have an account? <Link to='/register'>Register</Link></div>
 					</form>
-					<h4>42 Login</h4>
-					<a href="https://api.intra.42.fr/oauth/authorize?client_id=2d02a773dfbb227ded338c162245907ac4e2fa874a5e24a5e421aca1177cacab&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Flogin&response_type=code">login 42</a>
-					<h4>github Login</h4>
-					<a href="https://github.com/login/oauth/authorize?client_id=06dd042c7ba940906e5d&redirect_uri=http://localhost:3000/github/login">login github</a>
+					<h4>Log in via an API</h4>
+					<div><a href="https://api.intra.42.fr/oauth/authorize?client_id=2d02a773dfbb227ded338c162245907ac4e2fa874a5e24a5e421aca1177cacab&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Flogin&response_type=code">Log in via 42</a></div>
+					<div><a href="https://github.com/login/oauth/authorize?client_id=06dd042c7ba940906e5d&redirect_uri=http://localhost:3000/github/login">Log in via Github</a></div>
 				</div>
 			</div>
 		</main>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+console.log = console.warn = console.error = () => {};
+
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+		<App />,
 	document.getElementById('root')
 );
